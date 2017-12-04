5efe4f3b71d930192af4ae676b1e23945d795061
Quentin Bellow <qbellow@gmail.com>
2017-12-04 11:19:56 -0500

    Update QB_fizz_buzz.js

--- QB_fizz_buzz.js
+++ QB_fizz_buzz.js
@@ -1,4 +1,4 @@
-// Provides the range of numbers between 1 and 100
+// Provides the range of numbers between 1 and 100, wanted to practice developing
 
 var numRange = Array.apply(null, Array(100)).map(function (_, i) {return i + 1;});
 
@@ -17,4 +17,20 @@ for (var i = 0; i <= 100; i++){
     console.log("buzz");
   }
   else console.log(numRange[i]);
+}
+
+
+// Or, it can be done this way, skipping the extra step of creating the variable. 
+
+for (var i = 0; i <= 100; i++){
+  if (i % 3 === 0 && i % 5 ===0){
+    console.log("fizz buzz");
+  }
+  else if (i % 3 === 0){
+    console.log("fizz");
+  }
+  else if ( i % 5 === 0) {
+    console.log("buzz");
+  }
+  else console.log(i);
 }
